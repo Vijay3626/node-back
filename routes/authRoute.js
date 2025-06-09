6af5e31176a0b8a0c4125586163f3599eff69b15
Vijay3626 <vijaysekar3626@gmail.com>
2025-06-09 17:39:03 +0530

    test

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {  authLogin, authlogout, authregister } from "../controllers/authcontroller.js";
+import {  authget, authLogin, authlogout, authregister } from "../controllers/authcontroller.js";
 import { userList } from "../controllers/userController.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/login", authLogin)
 router.post("/register", authregister)
 router.post("/logout",authlogout)
 router.get("/userlist",userList);
+router.get("/get",authget)
 
 export default router;
